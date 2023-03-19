f9eda2f6e1511d34af337493a48daf18d6b23afd
generalsle1n <generalsle1n@github.com>
2023-03-19 19:13:32 +0100

    Added an Command Line Parser

--- FileCryptOperator/Program.cs
+++ FileCryptOperator/Program.cs
@@ -1,22 +1,93 @@
 ﻿using FileCryptClass;
-using System.Text;
+using FileCryptOperator;
+using System.CommandLine;
 
-string Password = "Secure";
-string IV = "Cool";
+string IVDefault = "abcdefghijklmnopqrstuvwxyz";
 
-using (FileCryptHelper fileCryptHelper = new FileCryptHelper(AESSize.Strong, Password, IV))
+RootCommand Main = new RootCommand();
+
+Option<string> Path = new Option<string>("--Path", "Specify the Path to operate")
 {
-    string folder = @"C:\temp\a\";
-    string[] Allfiles = Directory.GetFiles(folder);
+    IsRequired = true
+};
+Option<string> Password = new Option<string>("--Password", "Enter the password that was used to encrypt")
+{
+    IsRequired = true
+};
+Option<string> IV = new Option<string>("--IV", "Enter the iv value to randomize")
+{
+    IsRequired = false
+};
+Option<Mode> Mode = new Option<Mode>("--Mode", "Set the operate Mode (File or Folder)")
+{
+    IsRequired = true
+};
+Option<AESSize> Size = new Option<AESSize>("--Size", "Enter the length of the AES Key")
+{
+    IsRequired = true
+};
 
-    foreach(string File in Allfiles)
-    {
-        fileCryptHelper.EncryptFile(File);
-    }
+Command Decrypt = new Command("--Decrypt", "This Command decrypt files or folders");
+Decrypt.AddOption(Path);
+Decrypt.AddOption(Password);
+Decrypt.AddOption(IV);
+Decrypt.AddOption(Mode);
+Decrypt.AddOption(Size);
 
-    string[] AllNewfiles = Directory.GetFiles(folder);
-    foreach (string File in AllNewfiles)
+Decrypt.SetHandler((Path, Password, IV, Mode, Size) =>
+{
+    if (IV == null)
     {
-        fileCryptHelper.DecryptFile(File);
+        IV = IVDefault;
     }
-}
+    using (FileCryptHelper _helper = new FileCryptHelper(Size, Password, IV))
+    {
+        if (Mode == Mode.File)
+        {
+            _helper.DecryptFile(Path);
+        }
+        else if (Mode == Mode.Folder)
+        {
+            string[] AllFiles = Directory.GetFiles(Path);
+            foreach (string File in AllFiles)
+            {
+                _helper.DecryptFile(File);
+            }
+        }
+    }
+}, Path, Password, IV, Mode, Size);
+
+Main.AddCommand(Decrypt);
+
+Command Encrypt = new Command("--Encrypt", "This Command encrypt files or folders");
+Encrypt.AddOption(Path);
+Encrypt.AddOption(Password);
+Encrypt.AddOption(IV);
+Encrypt.AddOption(Mode);
+Encrypt.AddOption(Size);
+
+Encrypt.SetHandler((Path, Password, IV, Mode, Size) =>
+{
+    if(IV == null)
+    {
+        IV = IVDefault;
+    }
+    using (FileCryptHelper _helper = new FileCryptHelper(Size, Password, IV))
+    {
+        if (Mode == Mode.File)
+        {
+            _helper.EncryptFile(Path);
+        }
+        else if (Mode == Mode.Folder)
+        {
+            string[] AllFiles = Directory.GetFiles(Path);
+            foreach(string File in AllFiles)
+            {
+                _helper.EncryptFile(File);
+            }
+        }
+    }
+}, Path, Password, IV, Mode, Size);
+
+Main.AddCommand(Encrypt);
+Main.Invoke(args);
